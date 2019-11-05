b651d21872f23641942aa21809483c1a8e48dde2
roscas <sol.rosca@gmail.com>
2019-11-05 21:06:09 +0100

    initial commit

---
+++ json_generator.py
@@ -0,0 +1,29 @@
+import random
+
+import forgery_py, json, dataclasses
+from dataclasses import dataclass
+
+
+class EnhancedJSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if dataclasses.is_dataclass(o):
+            return dataclasses.asdict(o)
+        return super().default(o)
+
+
+@dataclass
+class Obj: id: str; description: str; name: str; price: str; photos: list
+
+
+description = lambda: forgery_py.lorem_ipsum.sentence()
+name = lambda: forgery_py.name.company_name()
+price = lambda: "{:.2f} chf".format(random.randint(1, 15) + random.random())
+photo = lambda: f'https://picsum.photos/id/{random.randint(100, 1000)}/800/600/'
+pics = lambda: [photo() for _ in range(3)]
+args = lambda: [description(), name(), price(), pics()]
+
+products = {'products': [Obj(str(i + 1), *args()) for i in range(100)]}
+
+app_json = json.dumps(products, cls=EnhancedJSONEncoder)
+
+print(app_json)
